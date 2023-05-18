af376890de221e92ad343d765840ee77bb456ae0
alireza10hp <45386926+alireza10hp@users.noreply.github.com>
2023-05-18 10:11:56 +0330

    multiple run with selenium

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ import time
 
 alpha = 1
 beta = 1.5
-
+n = 10
 
 if __name__ == '__main__':
     # Start 10 separate processes running register.py at the same time
     processes = []
-    for i in range(2):
+    for i in range(n):
         
         # using the weibullvariate() method
         t = random.weibullvariate(alpha, beta)
--- time.py
+++ time.py
@@ -3,11 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 capture = pyshark.FileCapture("selenium_traffic.pcap")
+#capture = pyshark.FileCapture("selenium_mult_10.pcap")
 #capture = pyshark.FileCapture("real_traffic.pcap")
 #capture = pyshark.FileCapture("2.pcap")
 i = 0
-interarrival = [0.0]*3000
-time = [0.0]*3000
+n = 3300
+interarrival = [0.0]*n
+time = [0.0]*n
 
 for packet in capture:
     
@@ -22,22 +24,22 @@ for j in range(1, i):
 #print("******")
 #print(i)
  
-#x = np.arange(0, 3000)
-#y = interarrival
+x = np.arange(0, n)
+y = interarrival
  
 # plotting
-#plt.title("Line graph")
-#plt.xlabel("X axis")
-#plt.ylabel("Y axis")
-#plt.plot(x, y, color ="blue")
-#plt.show()
-
-
-
-hist, bin_edges = np.histogram(interarrival)
-
-print(hist)
-print(bin_edges)
-
-plt.hist(interarrival, bins='auto')
+plt.title("Line graph")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.plot(x, y, color ="blue")
 plt.show()
+
+
+
+#hist, bin_edges = np.histogram(interarrival)
+
+#print(hist)
+#print(bin_edges)
+
+#plt.hist(interarrival, bins='auto')
+#plt.show()
